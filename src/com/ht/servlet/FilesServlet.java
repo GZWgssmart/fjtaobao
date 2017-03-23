57da0438eb223de8addaa4d918e30941e3f9ef02
CSWangBin <861543705@qq.com>
2017-03-23 11:31:24 +0800

    后台代码提交

--- src/com/ht/servlet/FilesServlet.java
+++ src/com/ht/servlet/FilesServlet.java
@@ -36,43 +36,11 @@ public class FilesServlet extends HttpServlet {
 
     private FilesService filesService;
     private ProductService productService;
-    private File excelFile;
-    private String excelFileFileName;
-    private ExcelWorkSheet<Product> excelWorkSheet;
-    private String fileName; // 文件名
-    private File file; // 用来保存上传的文件，file和表单file的name保持一致
-    private String fileFileName; // 用来保存上传的文件名，表单的file+FileName
-    private String filePath;
     private int total;
     private List<Files> rows;
 
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public File getFile() {
-        return file;
-    }
-
-    public void setFile(File file) {
-        this.file = file;
-    }
-
-    public String getFileFileName() {
-        return fileFileName;
-    }
-
-    public void setFileFileName(String fileFileName) {
-        this.fileFileName = fileFileName;
-    }
 
     public FilesServlet() {
-        excelWorkSheet = new ExcelWorkSheet<>();
-
         filesService = new FilesServiceImpl();
         productService = new ProductServiceImpl();
     }
@@ -80,9 +48,9 @@ public class FilesServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String method = WebUtil.getURIMethod(req);
-        if (method.equals("aa")) {
+        if (method.equals("queryAll")) {
             queryFiles(req, resp);
-        } else if (method.equals("bb")) {
+        } else if (method.equals("addFile")) {
             addFiles(req, resp);
         }
     }
@@ -138,22 +106,23 @@ public class FilesServlet extends HttpServlet {
                             files.setpStatus(fieldValue);
                         } else if (fieldName.equals("ftype")) {
                             files.setfType(fieldValue);
-                        } else if (fieldName.equals("fpath")) {
-                            files.setfPath(fieldValue);
                         }
                     } else { // 否则为文件字段
-                        System.out.printf("fsfsdfjsjfsjsjfdjsdf");
+                        System.out.printf("aaaaaaaaaaaaa");
+                        String fileName = fileItem.getName();// 获取文件的名称
                         String prefix = fileName.substring(fileName.lastIndexOf(".") + 1);
                         String newFileName = System.currentTimeMillis() + "." + prefix;
                         // 需要获取图片数据，把图片保存到本地服务器的某个目录里，并把路径保存到数据库
                         // 1、获取图片数据
                         InputStream in = fileItem.getInputStream(); // 获取文件输入流
-
+                        String filePath = null;
                         // 2、把图片数据保存到服务器的某个目录下
                         // new File(upload  + "/" + fileItem.getName())   上传的文件目录 + / + 文件名
                         if (fileName != null && !fileName.equals("")) {
+
                             filePath = Methods.getRootPath(req) + Methods.createNewFolder() + "\\" + newFileName;
                             FileUtils.copyInputStreamToFile(in, new File(filePath));
+                            System.out.printf(filePath + "cccccc");
                             // 3、把文件的路径保存到数据库
                             files.setName(fileName);
                             files.setfPath(filePath);
